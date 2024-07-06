4a0cb6f794d7efe63f12460b1d1b7847a64461e8
jignasha-fd <jignashaprajapati2010@gmail.com>
2024-07-06 22:55:44 +0530

    React-styling

---
+++ assignment/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Mui_Curd from './React-Assignment1/Practical/Mui_Curd'
+
+import StyleMUI from './React-Assignment1/Practical/StyleMUI'
+
+export default function App() {
+  return (
+    <div>
+      
+      <StyleMUI/>
+      {/* <Mui_Curd/> */}
+      
+    </div>
+  )
+}
